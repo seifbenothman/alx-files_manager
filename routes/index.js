141caa365ce876786acebfcf948e36d2ad1571a9
seifbenothman <seifbenothman95@gmail.com>
2024-06-29 01:38:12 +0000

    0

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,6 @@ import { Router } from 'express';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
 import AuthController from '../controllers/AuthController';
-import FilesController from '../controllers/FilesController';
 
 const router = Router();
 
@@ -12,6 +11,5 @@ router.post('/users', UsersController.postNew);
 router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
 router.get('/users/me', UsersController.getMe);
-router.post('/files', FilesController.postUpload);
 
 export default router;
