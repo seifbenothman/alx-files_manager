fb1789cec4f2d600b3e34049fdd86e65dfcb23a8
seifbenothman <seifbenothman95@gmail.com>
2024-06-29 00:31:53 +0000

    0

---
+++ controllers/UsersController.js
@@ -0,0 +1,29 @@
+import sha1 from 'sha1';
+import dbClient from '../utils/db';
+
+class UsersController {
+  static async postNew(req, res) {
+    const { email, password } = req.body;
+
+    if (!email) {
+      return res.status(400).json({ error: 'Missing email' });
+    }
+    if (!password) {
+      return res.status(400).json({ error: 'Missing password' });
+    }
+
+    const usersCollection = dbClient.db.collection('users');
+    const existingUser = await usersCollection.findOne({ email });
+
+    if (existingUser) {
+      return res.status(400).json({ error: 'Already exist' });
+    }
+
+    const hashedPassword = sha1(password);
+    const result = await usersCollection.insertOne({ email, password: hashedPassword });
+
+    return res.status(201).json({ id: result.insertedId, email });
+  }
+}
+
+export default UsersController;
